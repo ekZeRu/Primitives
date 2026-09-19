24fae302bf28fa1ba22e00f1ab5472fae9b1d642
Oleg <132949991+ekZeRu@users.noreply.github.com>
2026-09-20 00:49:23 +0300

    Add files via upload

---
+++ A.java
@@ -0,0 +1,5 @@
+package my.favourite.code;
+
+public class A {
+    public static  int x = 5;
+}
---
+++ Main.java
@@ -0,0 +1,92 @@
+import my.favourite.code.A;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+
+        char c = 33;
+        System.out.println(c);
+
+        boolean b = c == '!';
+
+        if (b) {
+            System.out.println("Hey");
+        }
+
+        /*float x = 3.5F;
+        double y = 0;*/
+        //System.out.println(x / y);
+
+        /*double d1 = 5 / y;
+        double d2 = 4 / y;
+        System.out.println(d1 / d2);*/
+
+        // 34534534534.34534534532
+        // 12 67t7867546546
+        // 0010010010110...001010101100
+        // 677867546546.345345342432434
+
+        // _ _ _ _ _
+
+        /*double d1 = 0.3 + 0.3 + 0.3;
+        double d2 = 0.9;
+        System.out.println(d1 == d2);
+        System.out.println(d1);
+        System.out.println(d2);
+
+        if (Math.abs(d2 - d1) < 0.1) {
+            System.out.println("Числа равны");
+        } else {
+            System.out.println("Числа не равны");
+        }*/
+
+        /*byte b1 = 1;
+        byte b2 = 2;
+        byte b3 = (byte)(b1 + b2);
+        System.out.println(b3);*/
+
+        /*int x = 10;
+        long xx = 100;
+        long y = x + xx;
+        System.out.println(y);*/
+
+        /*int i = 1;
+        while (true) {
+            System.out.println(i);
+            i *= 10; //i = i * 10;
+            Thread.sleep(300);
+        }*/
+
+        /*long x = 10;
+        int i = (int) x;
+        System.out.println(i);*/
+
+        /*int i = 10;
+        long x = i;*/
+        // 0 1 2 3 4 5 6 7 8 9 10
+        // 599 600
+
+        // 0 1 10 11 100 101
+
+        // 1 0 1 0
+        /*int x = 5; // 0 0 0 0 0 1 0 1*/
+        /*int x = -5; // 1 0 0 0 0 1 0 1
+        System.out.println(x);
+
+        byte b = 3;
+        short s = 15;
+        long q = 10_000_000_000L;*/
+
+
+        /*System.out.println("Hello World");
+        int x = 10;
+        if (x > 0) {
+            System.out.println("X больше нуля");
+        } else {
+            System.out.println("X не больше нуля");
+        }
+        System.out.println("Bye");
+
+        A.x;*/
+    }
+}
